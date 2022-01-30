0f6d8e25e8358a9403ed7447f21b9300d4ecc015
raj4angular <pokhraj.chaudhuri@gmail.com>
2022-01-30 11:31:06 +0530

    committing controller changes

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
                                       TimeUnit.MILLISECONDS.toMinutes(millis),
                                       TimeUnit.MILLISECONDS.toSeconds(millis) -
                                       TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-        return String.format("Hello, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+        return String.format("Hello123, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
     }
 
 }
